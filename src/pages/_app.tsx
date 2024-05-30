60c2b5fc86b18e3f47340ba77356b576ee3203de
rbueno <rafaelbuenolink@gmail.com>
2024-05-30 15:28:27 +0100

    general update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug()
+    },
+  })
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  
+
+  
+  return <>
+  <PostHogProvider client={posthog}>
+    <Component {...pageProps} />;
+  </PostHogProvider>
+  </>
 }
